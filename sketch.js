cf5cfe44c176706059a449e5731c6ec9fc667625
AKASH RAJ <akashraj9828@gmail.com>
2017-09-02 00:35:23 +0530

    unnecessary script removed
    extra varibles removed
    
    algo compacted
    
    debugging mode added

--- sketch.js
+++ sketch.js
@@ -1,71 +1,82 @@
 "use strict";
 
-var x = 1;
-var y = 1;
+var x = 1;		
+var y = 1;		
 var z = 1;
-var a = 25.9;//17.4  //default=10
-var b = 41;//47.9   //default=28
-var c = 7.8;//5.6 //default=8/3
-var dx = 1;
-var dy = 1;
-var dz = 1;
+var sigma = 25.9;//17.4  //default=10   //sigma	
+var rho = 41;//47.9   //default=28	//rho	
+var beta = 7.8;//5.6 //default=8/3		//beta
+var dx;
+var dy;
+var dz;
 var dt = 0.01;
-var re;
-var gr;
-var bl;
-var scl = 10;
+var scl;
 var px = [];
 var py = [];
 var pz = [];
 var i = 0;
 
+var getSigma;
+var getRho;
+var getBeta;
+
 var getX;
 var getY;
 var getZ;
-var rana;
-var ranb;
-var ranc;
+var get_dT;
+
+var debugging=true;
+var set_x_y_z=false;
+
 
 var scale_slider;
 
 
 function setup() {
-	createCanvas(windowWidth, windowHeight - 100);//,WEBGL);
+	createCanvas(windowWidth, windowHeight - 100);
 
 
 	background(51);
 
-	// var getX = prompt("Enter value for sigma:range(0,30)", 10);
-	// var getY = prompt("Enter value for rho:range(0,99)", 28);
-	// var getZ = prompt("Enter value for beta:range(0,29)", 2.6666);
-	// a= parseFloat(getX);
-	// b= parseFloat(getY);
-	// c= parseFloat(getZ);
-	console.log("a:" + a);
-	console.log("b:" + b);
-	console.log("c:" + c);
-	//var z = prompt("Enter value for Z:range(-40,40)", "1.9");
-	re = random(0, 255);
-	gr = random(0, 255);
-	bl = random(0, 255);
-	scale_slider = createSlider(0.01, 10, 1, 0.01);
-	scale_slider.changed(bg);
+	if(!debugging){
+	getSigma = prompt("Enter value for sigma:range(0,30)", 10);
+	getRho = prompt("Enter value for rho:range(0,99)", 28);
+	getBeta = prompt("Enter value for beta:range(0,29)", 2.6666);
+	sigma= parseFloat(getSigma);
+	rho= parseFloat(getRho);
+	beta= parseFloat(getBeta);
+	if(set_x_y_z){
+		getX = prompt("Enter value for X:range(0,20)", 1);
+		getY = prompt("Enter value for Y:range(0,29)", 1);
+		getZ = prompt("Enter value for Z:range(0,20)",1);
+		get_dT = prompt("Enter value for dT:range(0.01,0.2)", 0.01);
+		x= parseFloat(getX);
+		y= parseFloat(getY);
+		z= parseFloat(getZ);
+		dt=parseFloat(get_dT);
 
-	setInterval(function () { document.getElementById("info").innerHTML = "sigma:" + a + "\b\brho:" + b + "\b\bbeta:" + c}, 1000);
+	}
+	}
+	console.log("sigma:" + sigma);
+	console.log("rho:" + rho);
+	console.log("beta:" + beta);
+	
+
+	scale_slider = createSlider(0.1, 15, 5, 0.01);
+	scale_slider.position(100,100)	
 }
 
 
 function bg() {
 	background(51);
-
-
-
 }
+
+
 function setPoints() {
 
-	dx = (a * (y - x)) * dt;
-	dy = (x * (b - z) - y) * dt;
-	dz = ((x * y) - (c * z)) * dt;
+	dx = (sigma * (y - x)) * dt;
+	dy = (x * (rho - z) - y) * dt;
+	dz = ((x * y) - (beta * z)) * dt;
 	x += dx;
 	y += dy;
 	z += dz;
@@ -76,48 +87,55 @@ function setPoints() {
 
 function lines() {
 	var ranr;
-	
+
 	px[i] = x;
 	py[i] = y;
-	
-	colorMode(HSB);
-	
 
 	colorMode(HSB);
-	
 
-	for(var j=0;j<i;j+=1){
+
+	for (var j = 0; j < i; j += 1) {
 		push();
-		stroke(j/2%255, 255, 100);
-		strokeWeight(2/scl);
-		
-	beginShape();
-	
-	 vertex(px[j - 1], py[j - 1]);
-	 vertex(px[j], py[j]);
-	endShape();
-	pop();
-	
-	
+
+
+		stroke(j / 2 % 255, 255, 100);
+		strokeWeight(2 / scl);
+		beginShape();
+
+		vertex(px[j - 1], py[j - 1]);
+		vertex(px[j], py[j]);
+
+		endShape();
+
+		pop();
+
+
 	}
 	i++;
-	
+
 }
 
 
 
 function draw() {
 	background(51);
+	
+	
 	scl = scale_slider.value();
+	text('SCALE',50,37)
+	text(scl+'x',250,37)
 	scale(scl);
-
 	translate(width / 2 / scl, height / 2 / scl);
 	setPoints();
 	lines();
+
+	
+	
 	
 
+
 }
-	
+
 
 
 
