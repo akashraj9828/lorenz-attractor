026edbbec4eaa51f96d347c41ba3b96ad2a11cd8
AKASH RAJ <akashraj9828@gmail.com>
2017-09-03 19:10:02 +0530

    .min.js
    easy to understand
    
    fully commented

--- sketch.js
+++ sketch.js
@@ -30,24 +30,24 @@ var set_x_y_z=false;	//set true if want user input of inital x,y,z
 var shape_closed=true;	//set false if want to see orignal lorenz attractor
 
 
-var scale_slider;
+var scale_slider;		//slider variable for scaling
 
 
 
-function setup() {
+function setup() {			//setup function loads once only when webpage loads
 	createCanvas(windowWidth, windowHeight - 100);
 
 
-	background(51);
+	background(51);		//sets background to gray 
 
-	if(!debugging){
+	if(!debugging){     //when not debugging takes input (sigma,rho,beta) from user
 	getSigma = prompt("Enter value for sigma:range(0,30)", 10);
 	getRho = prompt("Enter value for rho:range(0,99)", 28);
 	getBeta = prompt("Enter value for beta:range(0,29)", 2.6666);
 	sigma= parseFloat(getSigma);
 	rho= parseFloat(getRho);
 	beta= parseFloat(getBeta);
-	if(set_x_y_z){
+	if(set_x_y_z){		////takes inital coordinate input(x,y,z) from user
 		getX = prompt("Enter value for X:range(0,20)", 1);
 		getY = prompt("Enter value for Y:range(0,29)", 1);
 		getZ = prompt("Enter value for Z:range(0,20)",1);
@@ -59,12 +59,14 @@ function setup() {
 
 	}
 	}
-	console.log("sigma:" + sigma);
+
+
+	console.log("sigma:" + sigma);  //you can see values of system varibles in console
 	console.log("rho:" + rho);
 	console.log("beta:" + beta);
 	
 
-	scale_slider = createSlider(0.1, 15, 5, 0.01);
+	scale_slider = createSlider(0.1, 15, 5, 0.01); 	//creating slider
 	scale_slider.position(100,100)	
 }
 
@@ -73,8 +75,10 @@ function bg() {
 	background(51);
 }
 
+///////////**ALGORITHM**////////////
 
-function setPoints() {
+
+function setPoints() {		//function to decide next (x,y,z) based on current (x,y,z)
 
 	dx = (sigma * (y - x)) * dt;
 	dy = (x * (rho - z) - y) * dt;
@@ -87,8 +91,7 @@ function setPoints() {
 
 
 
-function render_line() {
-	var ranr;
+function render_line() {		//function to draw line b/w all coordinates
 
 	px[i] = x;
 	py[i] = y;
@@ -97,15 +100,15 @@ function render_line() {
 
 
 	for (var j = 0; j < i; j +=10) {
-		push();
+	
 
 
 		stroke((i-j)/8% 255, 255, 100);
 		strokeWeight(2 / scl);
 		noFill();
-		beginShape();
+		beginShape();		//begins drawing of shape
 
-		vertex(px[j], py[j]);
+		vertex(px[j], py[j]);		//set vertices
 		vertex(px[j+1], py[j+1]);
 		vertex(px[j+2], py[j+2]);
 		vertex(px[j+3], py[j+3]);
@@ -119,37 +122,35 @@ function render_line() {
 		vertex(px[j+11], py[j+11]);
 
 		if(shape_closed)
-		endShape(CLOSE);
+		endShape(CLOSE);  	// **EXPERIMENTAL joins each 10th point to 1st point
 		else
-		endShape();
+		endShape();			//end of drawing shape
 
 
-		pop();
+		
 
 
 	}
-	i++;
+	i++;		//increments index of location array px[],py[],pz[]
 
 }
 
 
 
-function draw() {
-	background(51);
+function draw() {			//draw function gets called repeatedly to create amination
+	background(51);			//gray background
 	
 	
-	scl = scale_slider.value();  
+	scl = scale_slider.value();  	//sets scaling factor = slider value
 	text('SCALE',50,37)
 	text(scl+'x',250,37)
+
 	scale(scl);				// to zoom in or zoom out in canvas
-
 	translate(width / 2 / scl, height / 2 / scl); 		//translates from (0,0)[top-left corner] to (width/2,height/2)[center of canvas]
-	setPoints();
-	render_line();
-
 	
 	
-	
+	setPoints();		//calculate next point
+	render_line();		//draw line
 
 
 }
--- sketch_prepacked.js
+++ sketch_prepacked.js
@@ -1,14 +1,16 @@
-var setup, bg, setPoints, lines, draw, scale_slider, set_x_y_z, debugging, get_dT, getZ, getY, getX, getBeta, getRho, getSigma, i, pz, py, px, scl, dt, dz, dy, dx, beta, rho, sigma, z, y, x;
+var setup, bg, setPoints, render_line, draw, scale_slider, shape_closed, set_x_y_z, debugging, get_dT, getZ, getY, getX, getBeta, getRho, getSigma, i, pz, py, px, scl, dt, dz, dy, dx, beta, rho, sigma, z, y, x;
 (function () {
   "use strict";
 
   var _$0 = this;
 
   function _0() {
+    //setup function loads once only when webpage loads
     createCanvas(windowWidth, windowHeight - 100);
-    background(51);
+    background(51); //sets background to gray 
 
     if (!_$0.debugging) {
+      //when not debugging takes input (sigma,rho,beta) from user
       _$0.getSigma = prompt("Enter value for sigma:range(0,30)", 10);
       _$0.getRho = prompt("Enter value for rho:range(0,99)", 28);
       _$0.getBeta = prompt("Enter value for beta:range(0,29)", 2.6666);
@@ -17,6 +19,7 @@ var setup, bg, setPoints, lines, draw, scale_slider, set_x_y_z, debugging, get_d
       _$0.beta = _$0.parseFloat(_$0.getBeta);
 
       if (_$0.set_x_y_z) {
+        ////takes inital coordinate input(x,y,z) from user
         _$0.getX = prompt("Enter value for X:range(0,20)", 1);
         _$0.getY = prompt("Enter value for Y:range(0,29)", 1);
         _$0.getZ = prompt("Enter value for Z:range(0,20)", 1);
@@ -28,13 +31,14 @@ var setup, bg, setPoints, lines, draw, scale_slider, set_x_y_z, debugging, get_d
       }
     }
 
-    _$0.console.log("sigma:" + _$0.sigma);
+    _$0.console.log("sigma:" + _$0.sigma); //you can see values of system varibles in console
+
 
     _$0.console.log("rho:" + _$0.rho);
 
     _$0.console.log("beta:" + _$0.beta);
 
-    _$0.scale_slider = createSlider(0.1, 15, 5, 0.01);
+    _$0.scale_slider = createSlider(0.1, 15, 5, 0.01); //creating slider
 
     _$0.scale_slider.position(100, 100);
   }
@@ -44,6 +48,7 @@ var setup, bg, setPoints, lines, draw, scale_slider, set_x_y_z, debugging, get_d
   }
 
   function _2() {
+    //function to decide next (x,y,z) based on current (x,y,z)
     _$0.dx = _$0.sigma * (_$0.y - _$0.x) * _$0.dt;
     _$0.dy = (_$0.x * (_$0.rho - _$0.z) - _$0.y) * _$0.dt;
     _$0.dz = (_$0.x * _$0.y - _$0.beta * _$0.z) * _$0.dt;
@@ -53,44 +58,63 @@ var setup, bg, setPoints, lines, draw, scale_slider, set_x_y_z, debugging, get_d
   }
 
   function _3() {
-    var ranr;
+    //function to draw line b/w all coordinates
     _$0.px[_$0.i] = _$0.x;
     _$0.py[_$0.i] = _$0.y;
     colorMode(HSB);
 
-    for (var j = 0; j < _$0.i; j += 1) {
-      push();
-      stroke(j / 2 % 255, 255, 100);
+    for (var j = 0; j < _$0.i; j += 10) {
+      stroke((_$0.i - j) / 8 % 255, 255, 100);
       strokeWeight(2 / _$0.scl);
-      beginShape();
-      vertex(_$0.px[j], _$0.py[j]);
+      noFill();
+      beginShape(); //begins drawing of shape
+
+      vertex(_$0.px[j], _$0.py[j]); //set vertices
+
       vertex(_$0.px[j + 1], _$0.py[j + 1]);
-      endShape();
-      pop();
+      vertex(_$0.px[j + 2], _$0.py[j + 2]);
+      vertex(_$0.px[j + 3], _$0.py[j + 3]);
+      vertex(_$0.px[j + 4], _$0.py[j + 4]);
+      vertex(_$0.px[j + 5], _$0.py[j + 5]);
+      vertex(_$0.px[j + 6], _$0.py[j + 6]);
+      vertex(_$0.px[j + 7], _$0.py[j + 7]);
+      vertex(_$0.px[j + 8], _$0.py[j + 8]);
+      vertex(_$0.px[j + 9], _$0.py[j + 9]);
+      vertex(_$0.px[j + 10], _$0.py[j + 10]);
+      vertex(_$0.px[j + 11], _$0.py[j + 11]);
+      if (_$0.shape_closed) endShape(CLOSE); // **EXPERIMENTAL joins each 10th point to 1st point
+      else endShape(); //end of drawing shape
     }
 
-    _$0.i++;
+    _$0.i++; //increments index of location array px[],py[],pz[]
   }
 
   function _4() {
-    background(51);
-    _$0.scl = _$0.scale_slider.value();
+    //draw function gets called repeatedly to create amination
+    background(51); //gray background
+
+    _$0.scl = _$0.scale_slider.value(); //sets scaling factor = slider value
+
     text('SCALE', 50, 37);
     text(_$0.scl + 'x', 250, 37);
-    scale(_$0.scl);
-    translate(width / 2 / _$0.scl, height / 2 / _$0.scl);
+    scale(_$0.scl); // to zoom in or zoom out in canvas
 
-    _$0.setPoints();
+    translate(width / 2 / _$0.scl, height / 2 / _$0.scl); //translates from (0,0)[top-left corner] to (width/2,height/2)[center of canvas]
 
-    _$0.lines();
+    _$0.setPoints(); //calculate next point
+
+
+    _$0.render_line(); //draw line
+
   }
 
   setup = _0;
   bg = _1;
   setPoints = _2;
-  lines = _3;
+  render_line = _3;
   draw = _4;
   scale_slider = void 0;
+  shape_closed = void 0;
   set_x_y_z = void 0;
   debugging = void 0;
   get_dT = void 0;
@@ -128,4 +152,5 @@ var setup, bg, setPoints, lines, draw, scale_slider, set_x_y_z, debugging, get_d
   i = 0;
   debugging = true;
   set_x_y_z = false;
+  shape_closed = true;
 }).call(this);
